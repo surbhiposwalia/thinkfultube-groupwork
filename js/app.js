9621da66c386c703b6f21260dc43bca1aa38549b
surbhiposwalia <poswalia@ac87a31958e6.ant.amazon.com>
2016-08-29 18:42:02 -0700

    group work

--- js/app.js
+++ js/app.js
@@ -1,33 +1,40 @@
-$(function(){
+$(function() {
 
-  $('#search-form').submit(function(event) {
-    event.preventDefault();
-    var searchTerm = $('search-box').val();
-    $('search-box').val('');
-    getRequest(searchTerm);
-  });
+    $('#search-form').submit(function(event) {
+        event.preventDefault();
+        var searchTerm = $('#search-box').val();
+        $('#search-box').val('');
+        getRequest(searchTerm);
+    });
 
-  function getRequest(searchTerm) {
-    var params = {
-      part: 'snippet',
-      key: 'AIzaSyDLyjSGqBFk053JDmFljds0mbtXbuxAVJU',
-      q: searchTerm
-  };
+    $('body').on('click', '.show_channel_button', function() {
+        var channelName = $(this).text().slice(5, -7);
+        $('.result-list').empty();
+        getRequest(channelName);
+    });
 
-  $.getJSON('https://www.googleapis.com/youtube/v3/search', params,
-    function(data) {
-      var searchResult = data.items
-      console.log(searchResult);
-      $.each(searchResult, function(index, value) {
-        var picture = value.snippet.thumbnails.medium.url,
-            link = 'https://www.youtube.com/watch?v=' + value.id.videoId;
-        populateSearchResult(picture, link)
-      });
-    })
-}
+    function getRequest(searchTerm) {
+        var params = {
+            part: 'snippet',
+            q: searchTerm,
+            key: 'AIzaSyCCoDNOVP3ZAwlp6FP3FzZap3WhWTiaajI'
+        };
 
-  function populateSearchResult(pictureResult, linkResult) {
-    $('.result-list').append('<li class="result-item"><a href=' + linkResult + '><img src="' + pictureResult + '"></a><li>');
-  }
+        $.getJSON('https://www.googleapis.com/youtube/v3/search', params,
+            function(data) {
+                var searchResult = data.items;
+                $.each(searchResult, function(index, value) {
+                    var picture = value.snippet.thumbnails.medium.url,
+                        channelName = value.snippet.channelTitle,
+                        link = 'https://www.youtube.com/watch?v=' + value.id.videoId;
+                      populateSearchResult(picture, link, channelName);
+                });
+
+            })
+    }
+
+    function populateSearchResult(pictureResult, linkResult, channelNameResult) {
+        $('.result-list').append('<li class="result-item"><a href="' + linkResult + '"><img src="' + pictureResult + '"></a><button class="show_channel_button">Show ' + channelNameResult + ' videos</button></li>');
+    }
 
 });
